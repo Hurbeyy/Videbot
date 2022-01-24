76348bf2c17aa1f9d474725e50ce055eefc26c30
Hurbeyy <92626103+Hurbeyy@users.noreply.github.com>
2022-01-24 20:27:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,33 +1,26 @@
+# Copyright (C) 2021 By VeezMusicProject
+
 import os
-from os import getenv
+from os import path, getenv
 from dotenv import load_dotenv
 
-if os.path.exists("local.env"):
+if path.exists("local.env"):
     load_dotenv("local.env")
 
 load_dotenv()
-que = {}
-admins = {}
-SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME", "Veez Music")
-BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/20147c4f049e2c1f2f248.png")
-THUMB_IMG = getenv("THUMB_IMG", "https://telegra.ph/file/6809135960af808e931b9.png")
-AUD_IMG = getenv("AUD_IMG", "https://telegra.ph/file/7ffd2c88f5275fc9058eb.png")
-QUE_IMG = getenv("QUE_IMG", "https://telegra.ph/file/1e01db4b4bde83842e8d7.png")
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_USERNAME = getenv("BOT_USERNAME", "veezmusicbot")
-ASSISTANT_NAME = getenv("ASSISTANT_NAME", "veezassistant")
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "VeezSupportGroup")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "levinachannel")
-OWNER_NAME = getenv("OWNER_NAME", "dlwrml") # isi dengan username kamu tanpa simbol @
-PMPERMIT = getenv("PMPERMIT", None)
-OWNER_ID = int(os.environ.get("OWNER_ID")) # fill with your id as the owner of the bot
-DATABASE_URL = os.environ.get("DATABASE_URL") # fill with your mongodb url
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL")) # make a private channel and get the channel id
-BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False)) # just fill with True or False (optional)
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
-COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
-SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
-LANG = getenv("LANG", "id")
+
+class Veez(object):
+        admins = {}
+        BOT_TOKEN = getenv("BOT_TOKEN", None)
+        CHANNEL = int(os.getenv('CHANNEL', 123456))
+        API_ID = int(getenv("API_ID", "6"))
+        API_HASH = getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+        SESSION_NAME = getenv("SESSION_NAME", None)
+        DURATION_LIMIT = int(getenv("DURATION_LIMIT", "15"))
+        SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+        ASSISTANT_NAME = getenv("ASSISTANT_NAME", "tg_video_stream")
+        BOT_USERNAME = getenv("BOT_USERNAME", "veezvidstreambot")
+        COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
+        CHANNEL_NAME = getenv("CHANNEL_NAME", "levinachannel")
+        GROUP_NAME = getenv("GROUP_NAME", "VeezSupportGroup")
+        OWNER_NAME = getenv("OWNER_NAME", "dlwrml")
